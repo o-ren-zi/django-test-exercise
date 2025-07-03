4b94cfc64364346ad23c29a73139f87a69765fa3
1f10240036 <s1f102400369@iniad.org>
2025-07-03 13:38:12 +0900

    コードを整形

--- todo/tests.py
+++ todo/tests.py
@@ -9,7 +9,7 @@ from todo.models import Task
 class SampleTestCase(TestCase):
     def test_samplt1(self):
         self.assertEqual(1 + 2, 3)
-
+        
 
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
@@ -29,7 +29,7 @@ class TaskModelTestCase(TestCase):
         self.assertEqual(task.title, "task2")
         self.assertFalse(task.completed)
         self.assertEqual(task.due_at, None)
-        
+
     def test_is_overdue_future(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
         current = timezone.make_aware(datetime(2024, 6, 30, 0, 0, 0))
@@ -94,4 +94,3 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(response.context['tasks'][0], task1)
         self.assertEqual(response.context['tasks'][1], task2)
-        
